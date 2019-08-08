e40291ae06191d8e270b74ce5151eb5139e10450
Clinton Johnson <iamcmj@icloud.com>
2019-08-08 18:13:19 -0400

    updating scripts

--- main.py
+++ main.py
@@ -3,14 +3,8 @@
 import os
 import webbrowser
 import shutil
-# import optparse
-import random
-import string
-import click
-# import hashlib
-# import re
-# from multiprocessing import Pool
-# from cryptography.fernet import Fernet
+from multiprocessing import Pool
+
 
 apps_list = ['slack', 'webstorm', 'github desktop']
 
@@ -24,6 +18,10 @@ url = [
     'https://www.youtube.com/',
     'https://github.com/stockx/iron'
 ]
+
+
+def who_am_i():
+    return os.popen('whoami').read().strip()
 
 
 # Used for looping through urls and opening them in browser
@@ -52,69 +50,10 @@ def automate_file_creation(i):
             open_app()''')
         f.close()
         shutil.move(f"open_{i}.py", f"./apps/open_{i}.py")
-        # processes = f"open_{i}.py"
-        # with Pool(processes=3) as pool:
-        #     pool.map(run_process, processes)
-        # open_url(url)
+        processes = f"open_{i}.py"
+        with Pool(processes=3) as pool:
+            pool.map(run_process, processes)
+        open_url(url)
 
-
-def get_arguments():
-    return click.prompt('Enter the name of the apps you want Convinence to open')
-    # parser = optparse.OptionParser()
-    # parser.add_option("-n", "--name", dest="app_name", help="Enter name for the app you want to generate password for")
-    # (options, arguments) = parser.parse_args()
-    # if not options.open_apps:
-    #     parser.error('''
-    #     [-] Please enter the name of the app that you want to generate password for, and we will take care of the rest
-    #     ''')
-    # return options
-
-
-def create_random_password(for_length=10):
-    password_characters = string.ascii_letters + string.digits
-    return ''.join(random.choice(password_characters) for i in range(for_length))
-
-
-def encrypt_password():
-    return create_random_password(10)
-
-
-def return_pwd():
-    return os.popen('whoami').read().strip()
-
-
-def make_it(app_name):
-    if os.path.isfile(f"/Users/{return_pwd()}/desktop/conveniency_password_keeper.txt"):
-        with open(f"/Users/{return_pwd()}/desktop/conveniency_password_keeper.txt", "a+") as f:
-            f.write(f'\n{app_name}: {encrypt_password()}')
-            f.close()
-    else:
-        with open(f"password_keeper.txt", "w+") as f:
-            f.write(f'{app_name}: {encrypt_password()}')
-            f.close()
-            shutil.move("password_keeper.txt", f"/Users/{return_pwd()}/desktop/conveniency_password_keeper.txt")
-
-
-def main():
-    if os.path.isdir('./apps'):
-        print("Directory exist")
-    else:
-        app_name = get_arguments()
-        make_it(app_name)
-        # text = os.system('openssl rand -base64 16 | colrm 17')
-        # a_list = []
-        # a_list.append(text)
-        # print(a_list)
-
-        # os.mkdir('./apps')
-        # text = click.prompt('Enter the name of the apps you want Convinence to open', apps)
-        # apps_list.append(f"{text}")
-        # print(apps_list)
-        # for i in apps_list:
-        #     automate_file_creation(i)
-
-
-if __name__ == "__main__":
-    main()
 
 
